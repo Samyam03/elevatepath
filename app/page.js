3a58d913f13052bc7972423abe46c9f5f939aff7
Samyam03 <samyambhattarai03@gmail.com>
2025-05-18 20:33:03 -1100

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="outline">Button</Button>
+
+    </div>
+  );
+}
